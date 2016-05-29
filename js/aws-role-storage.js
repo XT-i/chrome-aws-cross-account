3bf544432dcb41d35945daaf2e208c6da4c3d889
Michael Willemse <michaekl.willemse@xt-i.com>
2016-05-29 22:53:15 +0200

    jquery library added

--- js/aws-role-storage.js
+++ js/aws-role-storage.js
@@ -17,17 +17,18 @@ AwsRoleStorage.prototype.initStorage = function(result){
     }
 };
 
-AwsRoleStorage.prototype.updateStorage = function(awsRoleStorage){
-    console.log(JSON.stringify(awsRoleStorage));
-    chrome.storage.sync.set({"awsRoleStorage": awsRoleStorage}, function(){console.log("AWS role storage updated")});
+AwsRoleStorage.prototype.updateStorage = function(){
+    console.log(JSON.stringify(this));
+    chrome.storage.sync.set({"awsRoleStorage": this}, function(){console.log("AWS role storage updated")});
 };
 
 AwsRoleStorage.prototype.addRole = function(role){
-    this.roles.add(role);
-    this.roles.sort(compareRoles)
+    this.roles.push(role);
+    this.roles.sort(compareRoles);
+    this.updateStorage();
 };
 
-AwsRoleStorage.prototype.deleteRole = function(role){
+AwsRoleStorage.prototype.deleteRole = function(index){
     if(this.roles.size > index){
         this.roles.splice(index, 1 );
     }
@@ -35,11 +36,11 @@ AwsRoleStorage.prototype.deleteRole = function(role){
 };
 
 AwsRoleStorage.prototype.addGroup = function(group){
-    this.groups.add(role);
+    this.groups.push(group);
     this.roles.sort(compareGroups)
 };
 
-AwsRoleStorage.prototype.deleteGroup = function(group){
+AwsRoleStorage.prototype.deleteGroup = function(index){
     if(this.groups.size > index){
         this.groups.splice(index, 1 );
     }
